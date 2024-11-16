5497b472d4db417484ca9c880860f1e1719de8bc
WANG-02-HKU <18830214865@163.com>
2024-11-16 16:14:30 +0800

    update

---
+++ assignment3.py
@@ -0,0 +1,14 @@
+from jetson.inference import detectNet
+from jetson.utils import videoSource.videoOutput
+net = detectNet("ssd-mobilenet-v2"，threshold=0.5)
+camera = videoSource("/home/nvidia/jetson-inference/data/images/humans_7.jpg")
+display = videoOutput("display://0")
+while display.IsStreaming():
+    img = camera.Capture()
+    if img is None:
+        continue
+    detections = net.Detect(img)
+    print(detections[0])
+
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.f} FPS".format(net.GetNetworkFPS()))
